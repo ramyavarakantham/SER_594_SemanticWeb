d7b318260ca7ae9a0eee32b500aaaede4632a90c
rasikachafekar <rasika.chafekar@gmail.com>
2017-11-06 21:13:16 -0700

    Addresses extracted to file addresses.txt

--- EventFul/get_EventArtist_pageLoop.py
+++ EventFul/get_EventArtist_pageLoop.py
@@ -1,10 +1,10 @@
-import urllib.request
+from urllib.request import Request, urlopen
 import json
 
 with open("artists.txt","r") as f:
     artistsList = f.read().splitlines()
 
-print(artistsList)
+#print(artistsList)
 
 url = "http://api.eventful.com/json/events/search?app_key=Tb4KWhVbXr4mK3Dp&keywords="
 
@@ -20,14 +20,17 @@ for i in range(0, len(artistsList)):
         for j in range(0, len(nameWords)):
             url = url +"+"+ nameWords[j]
             print(url)
-    page = urllib.request.urlopen(url)
-    if i == 0:
-        f = open("recordsByArtist.json", "w")
-    else:
-        f = open("recordsByArtist.json", "a")
+    page = urlopen(url)
 
-    #charset = page.info().get_content_charset()
-    content = page.read()
+    addresses = open("addresses.txt","a")
+    if i == 0:
+        f = open("recordsByArtistNew1.json", "w")
+    else:
+        f = open("recordsByArtistNew1.json", "a")
+
+        req = Request('http://api.eventful.com/json/events/search?app_key=Tb4KWhVbXr4mK3Dp&keywords=32',
+                      headers={'User-Agent': 'Mozilla/5.0'})
+    content = page.read().decode();
     #content1 = str(content, encoding='utf-8', errors='ignore')
 
     #content1 = content.decode('utf-8','replace')
@@ -38,11 +41,18 @@ for i in range(0, len(artistsList)):
     for j in range(1, int(NoOfRecs/10)+2):
         urlPage = url+"&page_number="+str(j)
         print(urlPage)
-        page=urllib.request.urlopen(urlPage)
-        content = page.read()
-        f.write(str(content))
+        page = urlopen(urlPage)
+        content = json.loads(page.read().decode())
+        if NoOfRecs/10 > 0.0:
+            events = content["events"]["event"]
+            for i in range(0, len(events)):
+                id = events[i]["id"]
+                venue = events[i]["venue_address"]
+                lat = events[i]["latitude"]
+                long = events[i]["longitude"]
+                toWrite="\n"+str(id)+","+str(venue)+","+str(lat)+","+str(long)
+                addresses.write(toWrite)
+        #json.dump(content, f)
     f.close()
 
 
-
-#The Administration
